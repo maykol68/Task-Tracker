56f4dd2ee3c80eb56bd65d15fc6ec86ca0aafb49
maykol68 <sm9365277@gmail.com>
2024-08-16 22:04:39 -0500

    last push

--- db/migrate/20240817030130_create_tasks.rb
+++ db/migrate/20240817030130_create_tasks.rb
@@ -1,9 +1,7 @@
 class CreateTasks < ActiveRecord::Migration[7.1]
   def change
     create_table :tasks do |t|
-      t.string :description
-      t.boolean :status
-      
+
       t.timestamps
     end
   end
